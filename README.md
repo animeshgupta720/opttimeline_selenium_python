51763fbe6192c0cc7554b00e95edcf97f7f4bfc0
Animesh Gupta <agupta4@snap.com>
2020-05-12 17:20:52 -0600

    changes

---
+++ README.md
@@ -0,0 +1,87 @@
+
+### opttimeline_selenium_python
+This project makes use of selenium module to perform webscraping on the USCIS website to check the OPT case status.
+
+### Prerequisites:
+```
+1. Install the following Python modules:
+
+- selenium - pip3 install selenium
+- smtplib - pip3 install smtplib
+
+2. Clone the project using  - *git clone https://github.com/animeshgupta720/opttimeline_selenium_python.git*
+
+Now you can run the script for two things - 1. to get just your own application status emailed 2. to get your own as well as a
+few other application status' printed on CLI.
+```
+### Running the script to get only your application status:
+
+When you execute the script *opt_atom.py*, it will prompt for the following inputs:
+
+1. Path where chromedriver is installed
+2. Your OPT application #
+3. Your email
+4. Your password (Don't worry your password is stored in local memory and sent for authentication over an encrypted channel 
+using SMTPS)
+
+Well, the *opt_atom.py* script will work fine if you run it manually; however, if you want the script to be run automatically
+in a periodic manner, you will have to use **CRON**.
+
+An example of a CRON job is mentioned below:
+
+**30 * * * *  cd /home/animesh && /usr/bin/python3 opt_atom.py >> /home/animesh/log.txt 2>&1**
+
+The above CRON job will execute every 30th minute of every hour in a day. 
+
+**Note:** If you want to set the script to execute periodically, then hard code the following inputs in the script:
+
+1. Make the path where chromedriver is installed static in your script 
+2. Your OPT application #
+3. Your email
+4. Your password input (Make sure that this script is not shared with anyone as you've hardcoded your password here)
+
+### Running the script to get your application status along with a few others sent close to your receipt date:
+
+Open the script and go to line #51 and #64 and make the following changes:
+
+#51 - Replace the range based on the range for which you want the data.
+#64 - Replace "YSC209015" with the first nine letters in your application id.
+
+For example, if your application id is this  - YSC2090163312; then make the following modifications to the opt_range.py file:
+
+-  Replace "for i in range(3200,3215)" to "for i in range(3300,3215)".
+
+-  Replace YSC209015 to YSC209016.
+
+# Output
+
+Now run the script to see an output like this:
+
+```
+Enter the path where chromedriver is installed:/Users/agupta4/Downloads/chromedriver
+Enter your OPT application number:YSC2090153214
+Enter your email: animeshgupta720@gmail.com
+Enter email password:
+
+YSC2090153200 Approved
+YSC2090153201 Approved 
+YSC2090153202 Approved 
+YSC2090153203 Received
+YSC2090153204 Approved 
+YSC2090153205 Approved 
+YSC2090153206 Approved 
+YSC2090153207 Received
+YSC2090153208 Received
+YSC2090153209 Approved 
+YSC2090153210 Approved 
+YSC2090153211 Received
+YSC2090153212 Received
+YSC2090153213 Received
+YSC2090153214 Received
+
+```
+
+
+
+
+
--- opt_atom.py
+++ opt_atom.py
@@ -1,74 +1,21 @@
 #!/usr/bin/env python3
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import os
 import time
 import re
 import smtplib, ssl
+import getpass
 
 path = input("Enter the path where chromedriver is installed:")
 my_id = input("Enter your OPT application number:")
-
+email = input("Enter your email: ")
+email_password = getpass.getpass("Enter email password: ")
 count=0
 count2=0
-for i in range (3200,3215):
-
- chrome_options = Options()
- chrome_options.add_argument('--headless')
- chrome_options.add_argument('--no-sandbox')
- chrome_options.add_argument('--disable-dev-shm-usage')
- #path = input("Enter the path where chromedrier is installed:")
- driver = webdriver.Chrome(path,options=chrome_options)
- #'/home/animesh/Downloads/chromedriver'
- driver.get("https://egov.uscis.gov/casestatus/landing.do")
-
-
- que = driver.find_element_by_xpath('//*[@id="receipt_number"]')
- que.send_keys("YSC209015"+ str(i))
- r_id = "YSC209015"+ str(i)
- time.sleep(3)
-
- que=driver.find_element_by_xpath('//*[@id="landingForm"]/div/div[1]/div/div[1]/fieldset/div[2]/div[2]/input')
- value = que.click()
- time.sleep(3)
- content = driver.page_source
-
- driver.quit()
-
- with open('webpage.html', 'w') as f:
-
-    f.write(content)
-
- with open('webpage.html','r') as f:
-    mystatus = f.read()
-
-
-
- r = re.compile(r'\bApproved\b | \bDelivered\b | \bMailed\b', flags=re.I | re.X)
- value = r.findall(mystatus)
-
- if 'Approved' in value  or 'Delivered' in value or 'Mailed' in value:
-    if r_id==my_id:
-     send_simple_message(my_id)
-     count = count+1
-     print(r_id,"Approved ")
-     time.sleep(2)
-    count = count+1
-    print(r_id,"Approved ")
-    time.sleep(2)
-     
- else:
-    print(r_id,"Received")
-    count2=count2+1
- 
-print("Total Approved = ",count)
-print("Total Received = ",count2)
-time.sleep(3)
-
-
 
 
 def send_simple_message(my_id):
@@ -76,9 +23,9 @@ def send_simple_message(my_id):
 
     smtp_server = "smtp.gmail.com"
     port = 587  # For starttls
-    sender_email = "animeshgupta720@gmail.com"
-    password = ""
-    receiver_email = "animeshgupta720@gmail.com"
+    sender_email = email
+    password = email_password
+    receiver_email = email
     # Create a secure SSL context
     context = ssl.create_default_context()
 
@@ -90,7 +37,7 @@ def send_simple_message(my_id):
         server.ehlo() # Can be omitted
         server.login(sender_email, password)
         # TODO: Send email here
-        message="Congratulations! Yor case status" + my_id + "is approved."
+        message="Congratulations! Your case status" + " " + my_id + " " + "is approved."
         server.sendmail(sender_email, receiver_email, message)
 
     except Exception as e:
@@ -99,5 +46,56 @@ def send_simple_message(my_id):
     finally:
         server.quit()
 
-#if x=='Approved' or 'Delivered':
-#    send_simple_message()
+
+
+
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+#path = input("Enter the path where chromedrier is installed:")
+driver = webdriver.Chrome(path,options=chrome_options)
+#'/home/animesh/Downloads/chromedriver'
+driver.get("https://egov.uscis.gov/casestatus/landing.do")
+
+
+que = driver.find_element_by_xpath('//*[@id="receipt_number"]')
+que.send_keys("my_id")
+time.sleep(3)
+
+que=driver.find_element_by_xpath('//*[@id="landingForm"]/div/div[1]/div/div[1]/fieldset/div[2]/div[2]/input')
+value = que.click()
+time.sleep(3)
+content = driver.page_source
+
+driver.quit()
+
+with open('webpage.html', 'w') as f:
+
+ f.write(content)
+
+with open('webpage.html','r') as f:
+ mystatus = f.read()
+
+
+
+r = re.compile(r'\bApproved\b | \bDelivered\b | \bMailed\b', flags=re.I | re.X)
+value = r.findall(mystatus)
+
+if 'Approved' in value  or 'Delivered' in value or 'Mailed' in value:
+
+ send_simple_message(my_id)
+ #count = count+1
+ #print(r_id,"Approved ")
+ time.sleep(2)
+count = count+1
+print(r_id,"Approved ")
+time.sleep(2)
+
+else:
+ print(r_id,"Received")
+ count2=count2+1
+
+print("Total Approved = ",count)
+print("Total Received = ",count2)
+time.sleep(3)
